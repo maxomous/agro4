1e524213079d1eae6dca47961e125879ba6ba99a
JamieDeakins <jamierdeakins@gmail.com>
2019-11-11 15:14:49 +0000

    max's changes

--- src/trees.c
+++ src/trees.c
@@ -11,7 +11,7 @@ tree_t trees[MAX_TREES];
 #define MAX_SPECIES 20
 species_t species[MAX_SPECIES];
 
-int hellomoto?;
+int Jamie, that was a shit change;
 int HELLLLLLLOOOOO;
 void addTree(tree_t t) {
 
